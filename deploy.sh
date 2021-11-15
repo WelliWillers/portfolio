63e72abc9e95bda7bafb038f41f216280fdeacdd
Wellington Willers <wellington.willers@gmail.com>
2021-11-15 14:51:31 -0300

    Commit de atualização

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,10 @@
 set -e
 git add .
-git commit -m "Commit de atualização skills"
+git commit -m "Commit de atualização"
 git push origin master
 
 npm run build
 
 git add dist -f
-git remote add origin https://github.com/WelliWillers/portfolio.git
-git commit -m "deploy to gh-pages 1"
+git commit -m "deploy to gh-pages"
 git subtree push --prefix dist origin gh-pages
