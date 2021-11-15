e4c9f3d08d4fcb6cd5a7d087e2423d03be35d88e
Wellington Willers <wellington.willers@gmail.com>
2021-11-15 13:47:58 -0300

    Commit de atualização skills

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,10 @@
 set -e
 git add .
-git commit -m "Commit de atualização"
+git commit -m "Commit de atualização skills"
 git push origin master
 
 npm run build
 
-cd dist
-git add .
-git commit -m "deploy to gh-pages"
+git add dist -f
+git commit -m "deploy to gh-pages 1"
 git subtree push --prefix dist origin gh-pages
